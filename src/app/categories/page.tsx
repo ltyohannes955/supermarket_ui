51521f3eb4a5ce930f5f406157b745ee5c15eefe
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-28 22:19:34 +0300

    created product and category pages

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/heder";
+
+export default function Categories() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
